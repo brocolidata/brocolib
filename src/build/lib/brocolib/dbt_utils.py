1f8027659e1f29c0ed075235edde2ecd26bafcb3
Amir B <amir.boutayeb@gmail.com>
2021-11-14 16:00:58 +0100

    0.0.2 dbt_utils

--- src/build/lib/brocolib/dbt_utils.py
+++ src/build/lib/brocolib/dbt_utils.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 
 
 def run_dbt_model(model, project_dir, profiles_dir):
@@ -15,8 +16,63 @@ def run_dbt_model(model, project_dir, profiles_dir):
     ls_commands = [
         "dbt", "run", 
         "--project-dir", project_dir,
-        "--profiles-dir", profiles_dir    
+        "--profiles-dir", profiles_dir,
+        "--select", f"+{model}+"
     ]
-    ls_commands.extend(["--models", f"+{model}+"])
-    output = subprocess.run(ls_commands, stdout=subprocess.PIPE)
+    output = run_subprocess(ls_commands)
     return output
+
+
+def stage_table(table, project_dir, profiles_dir):
+    """stage table from datalake to staging layer in dwh
+        and returns log
+
+    Args:
+        table (str): table to stage
+        project_dir (str): path to the dbt project_dir
+        profiles_dir (str): path to the dbt profiles_dir
+
+    Returns:
+        output (bytes): log of the staging
+    """
+    ls_commands = [
+        "dbt", "run-operation",
+        "stage_external_sources", 
+        "--project-dir", project_dir,
+        "--profiles-dir", profiles_dir,
+        "--args", f"\"select: {table}\""
+    ]
+
+    output = run_subprocess(ls_commands)
+    return output
+
+
+def run_subprocess(ls_commands):
+    """run command provided as arg and returns output
+
+    Args:
+        ls_commands (list): list of string representing the bash command to run
+
+    Returns:
+        (bytes): output of the bash command
+    """
+    out = ""
+    err = ""
+    ls_executable = [sys.executable, ls_commands]
+    
+    try:
+        process = subprocess.Popen(
+            ls_executable,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+        out, err = process.communicate()
+        
+        if process.returncode != 0:
+            return f"{out}\n{err} failed"
+
+    except Exception as e:
+        return str(e)
+
+    return f"{out}\n{err}"
