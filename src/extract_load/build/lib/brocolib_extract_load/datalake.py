42c0df2da29582cfb26b6183a1c96861eb9fd299
Amir B <amir.boutayeb@gmail.com>
2021-11-26 00:50:08 +0100

    v0.0.2 add support for parquet in eL

--- src/extract_load/build/lib/brocolib_extract_load/datalake.py
+++ src/extract_load/build/lib/brocolib_extract_load/datalake.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def dataframe_to_bucket(dataframe, bucket_name, blob_name, file_type, project_id=None):
+def dataframe_to_bucket(dataframe, bucket_name, blob_name, file_type):
   '''
   Function load dataframe to bucket
     - Create a blob representation of the dataframe
@@ -10,30 +10,30 @@ def dataframe_to_bucket(dataframe, bucket_name, blob_name, file_type, project_id
     dataframe (pandas.DataFrame): A dataframe   
     bucket_name (str): Name of the bucket 
     blob_name (str): Name of the blob you want to create
-    project_id(str, optional): ID of the project 
   
   Parameters example:
     blob_name= 'folder/subfolder/filename.csv'
     bucket_name = 'PROJECT_ID-landing'
 
   Returns:
-    dataframe (pandas.DataFrame): Transformed Dataframe
+    gcs_path (str): GCS path where the blob is uploaded
   '''
-  
-  file_type = file_type.lower()
-  url = f'gs://{bucket_name}/{blob_name}'
-  print(f'using {url}')
-  if file_type == 'csv':
-    
-    dataframe.to_csv(f'gs://{bucket_name}/{blob_name}',index=False)
+  gcs_path_temp = f"gs://{bucket_name}/{blob_name}/{{file_extension}}"
+  if file_type.lower() == 'csv':
+    gcs_path = gcs_path_temp.format(file_extension="csv")
+    dataframe.to_csv(gcs_path,index=False)
+  elif file_type.lower() == 'parquet':
+    gcs_path = gcs_path_temp.format(file_extension="parquet")
+    dataframe.to_parquet(gcs_path,index=False)
+  else:
+    return NotImplementedError(f"{file_type} is not implemented.")
+  return gcs_path
 
 
 
 def bucket_to_dataframe(bucket_name, blob_name, file_type):
-
     '''
-    Function writes a file into a dataframe
-      
+    Function writes a file into a dataframe     
 
     Parameters:
       bucket_name (str): Name of the bucket 
