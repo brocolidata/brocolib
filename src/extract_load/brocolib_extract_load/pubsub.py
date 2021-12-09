0b43247a70af5fa373c5e1df7bbb271695fe8a10
Amir B <amir.boutayeb@gmail.com>
2021-12-09 17:19:45 +0100

    logger is optional

--- src/extract_load/brocolib_extract_load/pubsub.py
+++ src/extract_load/brocolib_extract_load/pubsub.py
@@ -5,6 +5,7 @@ def publish_message_toPubSub(project_id, topic_id, message, logger):
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(project_id, topic_id)
     future = publisher.publish(topic_path, message)
-    logger.info(future.result())
+    if logger:
+        logger.info(future.result())
     
     return future.result()
