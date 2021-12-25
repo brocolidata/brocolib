b378445378528fed7d9c1847a677b047d1f01b6e
ic-33 <charbanou.ilias@gmail.com>
2021-12-25 21:29:19 +0100

    refactor EL

--- src/extract_load/setup.py
+++ src/extract_load/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 DESCRIPTION = 'Extract & Load Brocoli Library'
 LONG_DESCRIPTION = 'Brocoli Library for Data Extraction & Load'
 
