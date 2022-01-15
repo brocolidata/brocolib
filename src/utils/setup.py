042ab457989246ef3fc22335400c07ebf64f374b
Amir B <amir.boutayeb@gmail.com>
2022-01-15 01:38:57 +0100

    utils v0.0.15: fix client.bucket -> client.get_bucket

--- src/utils/setup.py
+++ src/utils/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.14'
+VERSION = '0.0.15'
 DESCRIPTION = 'Utils Brocoli Library'
 LONG_DESCRIPTION = 'Brocoli Collection of high level APIs'
 
