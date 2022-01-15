fcf55b42017b8d455a94a83d891dd8de48da2f04
Amir B <amir.boutayeb@gmail.com>
2022-01-15 02:09:56 +0100

    utils v0.0.17: fix metavar -> dest

--- src/utils/setup.py
+++ src/utils/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.16'
+VERSION = '0.0.17'
 DESCRIPTION = 'Utils Brocoli Library'
 LONG_DESCRIPTION = 'Brocoli Collection of high level APIs'
 
