54c073f9dba8687d96ce71729d2fb5e746124973
Amir B <amir.boutayeb@gmail.com>
2022-04-17 04:53:32 +0200

    added tests for ExternalTable

--- src/tests/extract_load/test_datalake.py
+++ src/tests/extract_load/test_datalake.py
@@ -1,11 +1,18 @@
 # contents of test_app.py, a simple test for our API retrieval
+from datetime import datetime
 import pytest
 import pandas as pd
 from requests import get
 from extract_load.brocolib_extract_load import datalake
 
 DEFAULT_BUCKET = "test-bucket"
-DEFAULT_BLOB_NAME = "folder/subfolder/file"
+DEFAULT_FILE = "test_file"
+DEFAULT_SUBFOLDERS = "macro"
+DEFAULT_TABLE_FOLDER = "table_folder"
+DEFAULT_BLOB_NAME = f"{DEFAULT_SUBFOLDERS}/{DEFAULT_TABLE_FOLDER}/{DEFAULT_FILE}"
+DEFAULT_DBT_TOPIC = "test_dbt_topic"
+DEFAULT_GCP_PROJECT = "default-gcp-project"
+DEFAULT_PARTITION_KEYS = {"year":"","month":""}
 
 ## -----| Fixtures |-----
 
@@ -48,3 +55,19 @@ def test_dataframe_to_bucket(mock_pandas, matrix_df, matrix_format):
         file_type=matrix_format
     )
     assert result == f"gs://{DEFAULT_BUCKET}/{DEFAULT_BLOB_NAME}.{matrix_format}"
+
+def test_external_table(mock_pandas):
+    external_table = datalake.ExternalTable(
+        bucket_name=DEFAULT_BUCKET,
+        partition_keys=DEFAULT_PARTITION_KEYS,
+        bucket_file=DEFAULT_FILE,
+        bucket_table_directory=DEFAULT_TABLE_FOLDER,
+        bucket_directory=DEFAULT_SUBFOLDERS,
+        dbt_topic=DEFAULT_DBT_TOPIC,
+        gcp_project=DEFAULT_GCP_PROJECT,
+    )
+    now = datetime.now()
+    path_prefix = f'{DEFAULT_SUBFOLDERS}/{DEFAULT_TABLE_FOLDER}'
+    path_partitions = f'year={now.year}/month={now.month}'
+    assert external_table.add_partition_keys(path_prefix) == f'{path_prefix}/{path_partitions}'
+    assert external_table.format_filename() == f'{path_prefix}/{path_partitions}/{DEFAULT_FILE}_{str(now.day)}'
