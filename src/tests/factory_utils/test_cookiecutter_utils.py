faf16d21ed30e1b507008561178d823f387b6565
ic-33 <charbanou.ilias@gmail.com>
2022-04-22 23:42:50 +0000

    added factory_utils to brolib_cd

--- src/tests/factory_utils/test_cookiecutter_utils.py
+++ src/tests/factory_utils/test_cookiecutter_utils.py
@@ -1,5 +1,5 @@
 import pytest
 
 def test_no_import_error():
-    from brocolib_factory_utils.cookiecutter_utils import cookiecut_template_cli
+    from brocolib_factory_utils import cookiecutter_utils
     pass
