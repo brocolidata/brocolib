8420d27eb8d42719e4b45908da27b6f94677af7f
ic-33 <charbanou.ilias@gmail.com>
2022-04-28 02:43:34 +0000

    test

--- src/factory_utils/setup.py
+++ src/factory_utils/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 DESCRIPTION = 'Factory Utils Brocoli Library'
 LONG_DESCRIPTION = 'Brocoli Library for Factory workflow'
 
