07f280abf75057bac32410ad2372af3e05eef205
Amir B <amir.boutayeb@gmail.com>
2022-11-11 15:32:02 +0100

    fix(factory_utils): root folder in cookiecutter
    - fix additional folder level in folders created by cookiecutter
    - add manual tests

---
+++ src/tests/factory_utils/manual_test_cookiecutter_utils.py
@@ -0,0 +1,5 @@
+import pytest
+
+def test_no_import_error():
+    from brocolib_factory_utils import cookiecutter_utils
+    pass
---
+++ src/tests/factory_utils/test_cookiecutter.py
@@ -0,0 +1,19 @@
+from brocolib_factory_utils.cookiecutter_utils import cookiecut_template
+
+CLIENT_REPO="dataplatform_functions"
+CLIENT_GITHUB_TOKEN="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"
+CLIENT_ORGANISATION="brocolidata-demo"
+LOCAL_DIR="./dataplatform_functions"
+BROCOLIB_GITHUB_TOKEN="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"
+BROCOLI_ORGANISATION="brocolidata"
+BROCOLI_TEMPL_REPO="factory_templates"
+COOKIE_TEMP_DIRECTORY="dataplatform_functions"
+
+
+cookiecut_template.cookiec_from_temp(
+    templ_repo=BROCOLI_TEMPL_REPO,
+    local_dir=".",
+    source_token=BROCOLIB_GITHUB_TOKEN,
+    source_organisation=BROCOLI_ORGANISATION, 
+    directory_name=CLIENT_REPO
+)
