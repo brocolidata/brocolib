3d3b3bd98a2f42836ff8418f569a1239d2fc434d
Amir B <amir.boutayeb@gmail.com>
2022-12-23 05:53:34 +0100

    fix code triggered with import

--- brocolib/factory_utils/tests/manual_test_cookiecutter.py
+++ brocolib/factory_utils/tests/manual_test_cookiecutter.py
@@ -9,11 +9,12 @@ BROCOLI_ORGANISATION="brocolidata"
 BROCOLI_TEMPL_REPO="factory_templates"
 COOKIE_TEMP_DIRECTORY="dataplatform_functions"
 
+if __name__ == '__main__':
 
-cookiecut_template.cookiec_from_temp(
-    templ_repo=BROCOLI_TEMPL_REPO,
-    local_dir=".",
-    source_token=BROCOLIB_GITHUB_TOKEN,
-    source_organisation=BROCOLI_ORGANISATION, 
-    directory_name=CLIENT_REPO
-)
+    cookiecut_template.cookiec_from_temp(
+        templ_repo=BROCOLI_TEMPL_REPO,
+        local_dir=".",
+        source_token=BROCOLIB_GITHUB_TOKEN,
+        source_organisation=BROCOLI_ORGANISATION, 
+        directory_name=CLIENT_REPO
+    )
