a4e235fec8fa3f420d3773a9f0678cd43b4e9ce9
Amir B <amir.boutayeb@gmail.com>
2022-12-23 01:58:21 +0100

    fix demo data file absolute path in tests
    - resolve absolute path in tests

--- brocolib/utils/tests/test_new_generator.py
+++ brocolib/utils/tests/test_new_generator.py
@@ -8,7 +8,8 @@ from brocolib_utils.fast_dbt import new_generator
 from brocolib_utils.fast_dbt.ddm import sheet_parser
 from brocolib_utils.fast_dbt.ddm import settings as codegen_settings
 
-DATA_FILES_FOLDER = "ddm_sheet_demo"
+DDM_SHEETS_FOLDER = "ddm_sheet_demo"
+DBT_MANIFESTS_FOLDER = "dbt_manifests_demo"
 DATA_FILES = [
         "SOURCE_TABLES",
         "SOURCE_COLUMNS"
@@ -20,7 +21,7 @@ def load_demo_data():
     sheet = sheet_parser.get_ddm_sheet()
     for str_sheet_name in DATA_FILES:
         parent_dir = pathlib.Path(__file__).parent.resolve()
-        base_file_path = os.path.join(parent_dir, DATA_FILES_FOLDER, f"{str_sheet_name}_filled.json")
+        base_file_path = os.path.join(parent_dir, DDM_SHEETS_FOLDER, f"{str_sheet_name}_filled.json")
         with open(base_file_path) as f:
             base_data = json.load(f)
         sheet_name = codegen_settings.get_sheet_enum(str_sheet_name)
@@ -46,7 +47,9 @@ def test_generate_source_yaml_asdict(load_demo_data, populate_bucket):
         source_name="test_source",
         datalake_bucket=bucket.name
     )
-    with open('/brocolib/tests/utils/dbt_manifests_demo/sources.yaml') as ff:
+    parent_dir = pathlib.Path(__file__).parent.resolve()
+    test_file_path = os.path.join(parent_dir, DBT_MANIFESTS_FOLDER, "sources.yaml")
+    with open(test_file_path) as ff:
         test_dict = yaml.safe_load(ff)
     assert sources_dict["sources"][0]["name"] == test_dict["sources"][0]["name"] 
     assert sources_dict["sources"][0]["tables"][0]["name"] == test_dict["sources"][0]["tables"][0]["name"]
